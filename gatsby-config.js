4543765dfd1c9ff1163c2962cce298392c4f174a
insidae <insidae@gmail.com>
2020-09-14 18:50:40 +0100

    Big dependency update commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,7 +42,8 @@ module.exports = {
           {
             resolve: "gatsby-remark-images",
             options: {
-              maxWidth: 840
+              maxWidth: 840,
+              linkImagesToOriginal: true
             }
           },
           {
