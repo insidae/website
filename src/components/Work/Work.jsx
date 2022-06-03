8122d20f8eb1b2f4acee959a8a1a0df709216722
insidae <insidae@gmail.com>
2022-06-03 15:18:00 +0100

    Adding Universal Paperclips to 'Work' page

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -8,7 +8,13 @@ class Work extends Component {
           <h1>
             Work
           </h1>
-          <p>A more comprehensive portfolio of my work is available on request.</p>
+          <p>
+            A more comprehensive portfolio of my work is available on request. If you are actually looking for work (as in, something to do), why not try your hand at making some paperclips.
+          </p>
+          
+          <div className="paperclips-iframe">
+                <iframe title="paperclips" src="/paperclips/" style={{ width: "100%", height: "100%", border: "none" }} />
+            </div>
         </div>
       </div>
     );
